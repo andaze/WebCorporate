a1af0515810ea2c62e619fce0641ee17883c4b77
Hata Tomoyuki （畠 智之） <hatayuki@andaze.com>
2022-04-08 14:57:22 +0900

    #77 change size of loading area

--- themes/andaze/assets/js/top.js
+++ themes/andaze/assets/js/top.js
@@ -216,8 +216,10 @@ img.addEventListener("load", () => {
   objects.push( mesh );
 
 
-  // 画面が読み込まれた後にロード画面を非表示
+  // 画面が読み込まれた後にフェードイン開始
   window.setTimeout(() => {
+
+    // ロード画面を非表示
     const spinner = document.getElementById('loading');
     spinner.style.opacity = 0;
     spinner.style.visibility = "hidden";
