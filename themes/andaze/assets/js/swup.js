67499ebc9c9d0fcd87ed0a0a204bf77cabf3eaa3
Nishikawa Daisaku <nishidai@andaze.com>
2022-05-29 19:28:18 +0900

    add swup extension plugin - swup-morph - fixed ページ遷移をスムースに（早い且つスムースでUX向上） #103

--- themes/andaze/assets/js/swup.js
+++ themes/andaze/assets/js/swup.js
@@ -2,11 +2,23 @@ import Swup from 'swup';
 import SwupBodyClassPlugin from '@swup/body-class-plugin';
 import SwupHeadPlugin from '@swup/head-plugin';
 import SwupHtmlLangPlugin from '@mashvp/swup-html-lang-plugin';
+import SwupScrollPlugin from '@swup/scroll-plugin';
+import SwupMorphPlugin from 'swup-morph-plugin';
 
 const swup = new Swup({
     plugins: [
         new SwupBodyClassPlugin(),
         new SwupHeadPlugin(),
-        new SwupHtmlLangPlugin()
+        new SwupHtmlLangPlugin(),
+        new SwupScrollPlugin({
+            doScrollingRightAway: true,
+            animateScroll: true
+        }),
+        new SwupMorphPlugin({
+            containers: [
+                '#nav-footer',
+                '#header_nav',
+            ]
+        }),
     ]
 });
