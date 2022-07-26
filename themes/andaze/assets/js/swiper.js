08fbac80b8460e834f388e3925b297550912d9c0
TomoyukiHata <hatayuki-andaze.com>
2022-07-26 11:00:00 +0900

    update logic to show swiper

--- themes/andaze/assets/js/swiper.js
+++ themes/andaze/assets/js/swiper.js
@@ -1,73 +1,94 @@
 import Swiper from 'swiper/bundle';
 
 export function activeSwiper() {
-    window.addEventListener('resize', function () { 
-        "use strict";
-    });
-    var sliderView;
-    if(screen.width > 320 && screen.width < 412)
-    {
-        sliderView = 1;
-    }
-    else if(screen.width > 413 && screen.width < 851)
-    {
-        sliderView = 2;
-    }
-    else
-    {
-        sliderView = 3; 
-    }
-    var newsSwiper = new Swiper(".newsSwiper", {
-        slidesPerView: sliderView,
-        spaceBetween: 30,
-        navigation: {
-            nextEl: ".newsNext",
-            prevEl: ".newsPrevious",
-        },
-    });
-    var casestudySwiper = new Swiper(".casestudySwiper", {
-        slidesPerView: sliderView,
-        spaceBetween: 30,
-        navigation: {
-            nextEl: ".casestudyNext",
-            prevEl: ".casestudyPrevious",
-        },
-    });
-    var sustainabilitySwiper = new Swiper(".sustainabilitySwiper", {
-        slidesPerView: sliderView,
-        spaceBetween: 30,
-        navigation: {
-            nextEl: ".sustainabilityNext",
-            prevEl: ".sustainabilityPrevious",
-        },
-    });
+    const breakPoint = 412;
 
-    var bar = document.querySelector('.progressbar_in');
-    var speed = 5000;
-    var sustainabilitySwiper = new Swiper(".professionalSwiper", {
-        slidesPerView: sliderView,
-        spaceBetween: 30,
-        on: {
-            slideChangeTransitionStart: function () {
-                bar.style.transitionDuration = '0s',
-                bar.style.transform = 'scaleX(0)'
+    let sustainabilitySwiper;
+    let sustainabilitySwiperBool;
+
+    window.addEventListener('load',()=>{
+        createSustainabilitySwiper();
+        if (breakPoint < window.innerWidth){
+            sustainabilitySwiperBool = false;
+        } else {
+            sustainabilitySwiperBool = true;
+        }
+    },false);
+
+    window.addEventListener('resize',()=>{
+        if (breakPoint < window.innerWidth && sustainabilitySwiperBool){
+            sustainabilitySwiper.destroy(false,true);
+            sustainabilitySwiperBool = false;
+        } else if (breakPoint >= window.innerWidth && !(sustainabilitySwiperBool)){
+            createSustainabilitySwiper();
+            sustainabilitySwiperBool = true;
+        }
+    },false);
+
+
+    // var newsSwiper = new Swiper(".newsSwiper", {
+    //     slidesPerView: sliderView,
+    //     spaceBetween: 30,
+    //     navigation: {
+    //         nextEl: ".newsNext",
+    //         prevEl: ".newsPrevious",
+    //     },
+    // });
+    // var casestudySwiper = new Swiper(".casestudySwiper", {
+    //     slidesPerView: sliderView,
+    //     spaceBetween: 30,
+    //     navigation: {
+    //         nextEl: ".casestudyNext",
+    //         prevEl: ".casestudyPrevious",
+    //     },
+    // });
+    const createSustainabilitySwiper = () => {
+        sustainabilitySwiper = new Swiper(".sustainabilitySwiper", {
+            breakpoints: {
+                280: {
+                    slidesPerView: 1,
+                },
+                412: {
+                    slidesPerView: 2,
+                },
+                851: {
+                    slidesPerView: 3,
+                }
             },
-            slideChangeTransitionEnd: function () {
-                bar.style.transitionDuration = speed + 'ms',
-                bar.style.transform = 'scaleX(1)'
+            spaceBetween: 30,
+            navigation: {
+                nextEl: ".sustainabilityNext",
+                prevEl: ".sustainabilityPrevious",
             },
-        },
-        navigation: {
-            nextEl: ".professionalNext",
-            prevEl: ".professionalPrevious",
-        },
-        autoplay: {
-            delay: 5000,
-            disableOnInteraction: false,
-        },
-        loop: true,
-        loopAdditionalSlides: 1,
-        speed: 800,
-    });
+        });
+    }
+
+    // var bar = document.querySelector('.progressbar_in');
+    // var speed = 5000;
+    // var sustainabilitySwiper = new Swiper(".professionalSwiper", {
+    //     slidesPerView: sliderView,
+    //     spaceBetween: 30,
+    //     on: {
+    //         slideChangeTransitionStart: function () {
+    //             bar.style.transitionDuration = '0s',
+    //             bar.style.transform = 'scaleX(0)'
+    //         },
+    //         slideChangeTransitionEnd: function () {
+    //             bar.style.transitionDuration = speed + 'ms',
+    //             bar.style.transform = 'scaleX(1)'
+    //         },
+    //     },
+    //     navigation: {
+    //         nextEl: ".professionalNext",
+    //         prevEl: ".professionalPrevious",
+    //     },
+    //     autoplay: {
+    //         delay: 5000,
+    //         disableOnInteraction: false,
+    //     },
+    //     loop: true,
+    //     loopAdditionalSlides: 1,
+    //     speed: 800,
+    // });
     
 }
