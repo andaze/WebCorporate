20cfee2bf0e1ca24eb9a2fd84585ba13ad777e8f
TomoyukiHata <hatayuki-andaze.com>
2022-07-26 11:13:29 +0900

    update logic to show newsSwiper

--- themes/andaze/assets/js/swiper.js
+++ themes/andaze/assets/js/swiper.js
@@ -4,9 +4,11 @@ export function activeSwiper() {
     const breakPoint = 412;
 
     let sustainabilitySwiper;
+    let newsSwiper;
     let swiperBool;
 
     window.addEventListener('load',() => {
+        createNewsSwiper();
         createSustainabilitySwiper();
         if (breakPoint < window.innerWidth){
             swiperBool = false;
@@ -17,23 +19,37 @@ export function activeSwiper() {
 
     window.addEventListener('resize',()=>{
         if (breakPoint < window.innerWidth && swiperBool){
+            newsSwiper.destroy(false,true);
             sustainabilitySwiper.destroy(false,true);
             swiperBool = false;
         } else if (breakPoint >= window.innerWidth && !(swiperBool)){
+            createNewsSwiper();
             createSustainabilitySwiper();
             swiperBool = true;
         }
     },false);
 
+    const createNewsSwiper = () => {
+        newsSwiper = new Swiper(".newsSwiper", {
+            breakpoints: {
+                280: {
+                    slidesPerView: 1,
+                },
+                412: {
+                    slidesPerView: 2,
+                },
+                851: {
+                    slidesPerView: 3,
+                }
+            },
+            spaceBetween: 30,
+            navigation: {
+                nextEl: ".newsNext",
+                prevEl: ".newsPrevious",
+            },
+        });
+    }
 
-    // var newsSwiper = new Swiper(".newsSwiper", {
-    //     slidesPerView: sliderView,
-    //     spaceBetween: 30,
-    //     navigation: {
-    //         nextEl: ".newsNext",
-    //         prevEl: ".newsPrevious",
-    //     },
-    // });
     // var casestudySwiper = new Swiper(".casestudySwiper", {
     //     slidesPerView: sliderView,
     //     spaceBetween: 30,
