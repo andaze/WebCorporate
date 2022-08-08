124d6607f5d47d01a7cefd2c409a513b1fc37b69
TomoyukiHata <hatayuki-andaze.com>
2022-08-08 21:52:18 +0900

    revert and add console to top.js

--- themes/andaze/assets/js/top.js
+++ themes/andaze/assets/js/top.js
@@ -2,7 +2,8 @@ import * as THREE from 'three';
 import * as TWEEN from '@tweenjs/tween.js';
 import gsap from 'gsap';
 
-
+console.log(location.pathname);
+console.log(location.href);
 // ---------------------------------------------------------------------------------------------
 // ローディング画面の表示
 // ---------------------------------------------------------------------------------------------
