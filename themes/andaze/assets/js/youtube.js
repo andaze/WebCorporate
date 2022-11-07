19d61277bfe6ff1db708879218f79eaacd93bee2
TomoyukiHata <hatayuki-andaze.com>
2022-11-07 11:44:09 +0900

    fix for responsive

--- themes/andaze/assets/js/youtube.js
+++ themes/andaze/assets/js/youtube.js
@@ -1,10 +1,6 @@
 export function handleYoutube() {
-    let contentNum;
-    if (window.innerWidth > 280 && window.innerWidth < 851 ) {
-        contentNum = 2;
-    } else {
-        contentNum = 3;
-    }
+    let contentNum = 3;
+
     $.ajax({
         type: 'GET',
         url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCfjT9TH2TOy7QRp1X_Ibr4A&maxResults=' + contentNum.toString() + '&order=date&type=video&key=AIzaSyCQdG7SoPSJVX-vhkkzhjSB6luh6a8Kyno',
@@ -17,7 +13,7 @@ export function handleYoutube() {
                 var image = json.items[i].snippet.thumbnails.medium.url;
                 var title = json.items[i].snippet.title;
                 $("#youtubeList").append(
-                    '<div class="w-[31.3%]"><img class="h-[180px] w-full object-cover m-0 js-modal-video" src="' + image + '" data-video-id="' + ID + '"><p class="font-bold pt-5 text-sm">' + title + '</p></div>'
+                    '<div class="w-full lg:w-[31.3%] mb-8 sm:mb-0"><img class="h-[180px] w-full object-cover m-0 js-modal-video" src="' + image + '" data-video-id="' + ID + '"><p class="font-bold pt-5 text-sm">' + title + '</p></div>'
                 );
             }
         },
