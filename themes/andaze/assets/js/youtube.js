1b6bf00abfa0d9c197523a27a36a8a3bfcc0522f
TomoyukiHata <hatayuki-andaze.com>
2022-11-10 15:27:18 +0900

    remove jQuery

--- themes/andaze/assets/js/youtube.js
+++ themes/andaze/assets/js/youtube.js
@@ -1,23 +1,8 @@
 export class handleYoutube {
 
   constructor() {
-    this.showModalVideo();
     this.showCompanyMovie();
   }
-
-    // YouTubeチャンネル動画の取得
-    showModalVideo() {
-        if ($(".js-modal-video").length) { //クラス名js-modal-videoがあれば以下を実行
-            $(".js-modal-video").modalVideo({
-                channel: "youtube",
-                youtube: {
-                    rel: 0, //関連動画の指定
-                    autoplay: 1, //自動再生の指定
-                    controls: 0, //コントロールさせるかどうかの指定
-                },
-            });
-        }
-    }
 
     // 会社説明動画用
     showCompanyMovie() {
