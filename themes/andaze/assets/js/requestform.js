c546fca7f735039850bf0c09a208caee29630fa9
TomoyukiHata <hatayuki-andaze.com>
2022-12-09 14:33:36 +0900

    add style and function for mobile

--- themes/andaze/assets/js/requestform.js
+++ themes/andaze/assets/js/requestform.js
@@ -2,13 +2,11 @@ export function handleForm() {
     const form = document.getElementById('accordion-collapse');
     const expander = document.querySelector('#accordion-collapse-heading-request > p');
     const expander_body = document.getElementById('accordion-collapse-body-request');
-    console.log(expander)
-    console.log(expander.getAttribute('aria-expanded'))
 
     let first_shown = true;
 
     form.style.opacity = 0;
-    form.style.visibility = "invisible";
+    form.style.visibility = "hidden";
 
     const key_visual = document.getElementById("key-visual");
     let key_visual_bottom = key_visual.getBoundingClientRect().bottom + window.pageYOffset;
@@ -16,33 +14,84 @@ export function handleForm() {
 
     var timeoutId;
 
-    window.addEventListener('scroll', () => {
-        if (window.scrollY >= target_static) {
+    if (window.innerWidth >= 900) {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY >= target_static) {
 
-            form.style.opacity = 0;
-            form.style.visibility = "invisible";
+                form.style.opacity = 0;
+                form.style.visibility = "hidden";
 
-            clearTimeout( timeoutId ) ;
+                clearTimeout( timeoutId ) ;
 
-            timeoutId = setTimeout( function () {
-                if (window.scrollY >= target_static) {
-                    form.style.opacity = 1;
-                    form.style.visibility = "visible";
-                    if (first_shown) {
-                        first_shown = !first_shown;
-                    } else if (!first_shown & expander.getAttribute('aria-expanded') === "false") {
-                        expander_body.classList.add("hidden");
+                timeoutId = setTimeout( function () {
+                    if (window.scrollY >= target_static) {
+                        form.style.opacity = 1;
+                        form.style.visibility = "visible";
+                        if (first_shown) {
+                            first_shown = !first_shown;
+                        } else if (!first_shown & expander.getAttribute('aria-expanded') === "false") {
+                            expander_body.classList.add("hidden");
+                        }
                     }
-                }
-            }, 500 ) ;
-        } else if (window.scrollY < target_static) {
-            form.style.opacity = 0;
-            form.style.visibility = "invisible";
-        }
-    });
+                }, 500 ) ;
+            } else if (window.scrollY < target_static) {
+                form.style.opacity = 0;
+                form.style.visibility = "hidden";
+            }
+        });
+    } else {
+        const button_wrapper = document.getElementById('request-form-mobile');
+        const button = document.querySelector('#request-form-mobile > button');
+        const original_text = button.textContent;
+        let clicked = false;
 
-    window.setInterval(() => {
-        console.log(first_shown)
-    }, 500)
+        const black_background = document.getElementById('black_background');
+
+
+        window.addEventListener('scroll', () => {
+
+            if (window.scrollY >= target_static) {
+                button_wrapper.style.opacity = 1;
+                button_wrapper.style.visibility = "visible";
+            } else {
+                button_wrapper.style.opacity = 0;
+                button_wrapper.style.visibility = "hidden";
+                black_background.style.visibility = "hidden";
+
+                form.style.opacity = 0;
+                form.style.visibility = "hidden";
+
+                if (clicked) {
+                    window.setTimeout(() => {
+                        clicked = false;
+                        button.textContent = original_text;
+                    }, 1000);
+                }
+
+            }
+        })
+        button.addEventListener('click', () => {
+
+            if (!clicked) {
+                form.style.opacity = 1;
+                form.style.visibility = "visible";
+
+                black_background.style.opacity = 0.6;
+                black_background.style.visibility = "visible";
+
+                clicked = true;
+                button.textContent = "閉じる";
+            } else {
+                form.style.opacity = 0;
+                form.style.visibility = "hidden";
+
+                black_background.style.opacity = 0;
+                black_background.style.visibility = "hidden";
+
+                clicked = false;
+                button.textContent = original_text;
+            }
+        });
+    }
 
 }
