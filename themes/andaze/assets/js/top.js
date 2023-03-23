ebb75928bc06eaa24bf65fdb3cd465332fa66e27
TomoyukiHata <hatayuki-andaze.com>
2023-03-23 14:39:53 +0900

    change diffusion setting

--- themes/andaze/assets/js/top.js
+++ themes/andaze/assets/js/top.js
@@ -596,8 +596,8 @@ async function initKeyVisual() {
     
       // パスがトップページ以外の場合、タブが非アクティブの場合、アニメーション停止
       if (
-        // !((location.pathname === "/ja/") | (location.pathname === "/en/"))
-        !((location.pathname === "/WebCorporate/ja/") | (location.pathname === "/WebCorporate/en/"))
+        !((location.pathname === "/ja/") | (location.pathname === "/en/"))
+        // !((location.pathname === "/WebCorporate/ja/") | (location.pathname === "/WebCorporate/en/"))
       ) {
         return;
       } else {
